6e93b3f219e614f3e65edeeedc0d4c33317d19d7
totto2727 <kaihatu.totto2727@gmail.com>
2022-08-27 11:28:02 +0900

    ランタイムの指定をフォームとサーバーに追加

--- control-panel-backend/main.go
+++ control-panel-backend/main.go
@@ -9,6 +9,12 @@ import (
 	"time"
 )
 
+const (
+	Binary  = "binary"
+	NODE_JS = "nodejs"
+	PYTHON  = "python"
+)
+
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	// 入力のバリデーション
 	file, fileHeader, err := r.FormFile("userfile")
@@ -21,8 +27,16 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	userID := r.FormValue("userid")
 	appName := r.FormValue("appname")
+	runtime := r.FormValue("runtime")
 	if len(userID) == 0 || len(appName) == 0 {
 		log.Println("Error:FormValue is Empty")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	switch runtime {
+	case Binary, NODE_JS, PYTHON:
+	default:
+		fmt.Println(err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
@@ -39,7 +53,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	// アプリケーションファイルの保存
 	fileName := fileHeader.Filename
 	datetime := time.Now().Format(time.RFC3339Nano)
-	dst, err := os.Create(fmt.Sprintf("%s/%s-%s", storagePath, datetime, fileName))
+	dst, err := os.Create(fmt.Sprintf("%s/%s", storagePath, datetime))
 	if err != nil {
 		fmt.Println(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -53,13 +67,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	fmt.Fprintf(w, "Upload successful")
-
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World")
+	fmt.Fprintf(w, "Upload successful:%s", fileName)
 }
 
 func handler_(w http.ResponseWriter, r *http.Request) {
@@ -69,8 +77,6 @@ func handler_(w http.ResponseWriter, r *http.Request) {
 func setupRoutes() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/upload", uploadHandler) // 追加
-	// mux.HandleFunc("/upload", handler_)
-	mux.HandleFunc("/", handler)
 
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Fatal(err)
