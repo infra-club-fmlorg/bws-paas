36fb3188cb248395f968c4341eeecccbcd2e854c
totto2727 <kaihatu.totto2727@gmail.com>
2022-09-09 03:28:04 +0900

    テスト用スクリプトの微調整

--- scripts/test/upload-html.sh
+++ scripts/test/upload-html.sh
@@ -2,4 +2,4 @@ cd $(dirname $0)
 
 zip -r html html-project -x \*/.git/\* \*/node_modules/\*
 
-docker run --rm --network=application-network -v $PWD/html.zip:/html.zip curlimages/curl -H "Host:secure.niij.fml.org" -X POST -F user_name=totto -F application_name=html-project -F runtime=html -F applicationfile=@html.js.zip nginx:80/api/1/control-panel-backend/upload
+docker run --rm --network=application-network -v $PWD/html.zip:/html.zip curlimages/curl -H "Host:secure.niij.fml.org" -X POST -F user_name=totto -F application_name=html-project -F runtime=html -F application_file=@html.zip nginx:80/api/1/control-panel-backend/upload
